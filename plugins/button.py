754b9069f1ef46b4a8a082bec4421f2cee91631d
Lakki reddy <pulka143341@gmail.com>
2025-06-30 18:45:03 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -1,20 +1,19 @@
-import logging 
-import asyncio 
-import json 
-import os 
-import shutil 
-import time from datetime 
-import datetime 
-from pyrogram 
-import enums from pyrogram.types 
-import InputMediaPhoto from plugins.config 
-import Config from plugins.script 
-import Translation from plugins.thumbnail 
-import * from plugins.functions.display_progress 
-import progress_for_pyrogram, humanbytes from plugins.database.database 
-import db from PIL 
-import Image from plugins.functions.ran_text 
-import random_char
+import logging
+import asyncio
+import json
+import os
+import shutil
+import time
+from datetime import datetime
+from pyrogram import enums
+from pyrogram.types import InputMediaPhoto
+from plugins.config import Config
+from plugins.script import Translation
+from plugins.thumbnail import *
+from plugins.functions.display_progress import progress_for_pyrogram, humanbytes
+from plugins.database.database import db
+from PIL import Image
+from plugins.functions.ran_text import random_char
 
 cookies_file = 'cookies.txt'
 
