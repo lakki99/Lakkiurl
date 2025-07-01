313c7b3c450c1e0f38ab21f6489d4aa9529891c4
Lakki reddy <pulka143341@gmail.com>
2025-07-01 12:29:21 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -66,8 +66,8 @@ async def echo(bot, update):
     if not update.from_user:
         return await update.reply_text("I don't know about you sar :(")
     await AddUser(bot, update)
-    if Config.UPDATES_CHANNEL:
     if update.chat.type == enums.ChatType.PRIVATE:
+    if Config.UPDATES_CHANNEL:
         fsub = await handle_force_subscribe(bot, update)
         if fsub == 400:
             return
